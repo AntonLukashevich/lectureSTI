52b8ff99751cb31c7d718e803d1fe5e984114229
Anton Lukashevich <luka14a@gmail.com>
2021-02-16 11:08:44 +0300

    ContentMigration

---
+++ db/migrate/20210216075048_add_content_columns_to_lecture_blocks.rb
@@ -0,0 +1,14 @@
+class AddContentColumnsToLectureBlocks < ActiveRecord::Migration[6.1]
+
+  def up
+    add_column :lecture_blocks, :text_content, :text
+    add_column :lecture_blocks, :image_content, :string
+    add_column :lecture_blocks, :video_content, :string
+  end
+
+  def down
+    remove_column :lecture_blocks, :text_content
+    remove_column :lecture_blocks, :image_content
+    remove_column :lecture_blocks, :video_content
+  end
+end
